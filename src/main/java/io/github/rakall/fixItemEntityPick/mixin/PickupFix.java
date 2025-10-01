3e79f3601b91f5174e0c9bdab3a1b022e77beadd
Ivan <135434946+Tsyhanok-Ivan@users.noreply.github.com>
2025-10-01 18:02:58 +0300

    Switch from @Inject to @WrapMethod for HopperBlockEntity.extract() to improve mod compatibility

--- src/main/java/io/github/rakall/fixItemEntityPick/mixin/PickupFix.java
+++ src/main/java/io/github/rakall/fixItemEntityPick/mixin/PickupFix.java
@@ -1,22 +1,20 @@
 package io.github.rakall.fixItemEntityPick.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import io.github.rakall.fixItemEntityPick.util.iPickupFix;
 import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.inventory.Inventory;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HopperBlockEntity.class)
 public class PickupFix implements iPickupFix {
-
-    @Inject(method = "extract(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/entity/ItemEntity;)Z", at = @At("HEAD"), cancellable = true)
-    private static void wrapExtract(Inventory inventory, ItemEntity itemEntity, CallbackInfoReturnable<Boolean> cir) {
+    @WrapMethod(method = "extract(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/entity/ItemEntity;)Z")
+        private static boolean wrapExtract(Inventory inventory, ItemEntity itemEntity, Operation<Boolean> original) {
         if (((GetPickupDelay) itemEntity).getPickupDelay() < 0) {
-            cir.setReturnValue(false);
+            return false;
         }
+        return original.call(inventory, itemEntity);
     }
-
 }
